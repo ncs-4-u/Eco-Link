0c2e22eed3852e3a92efc1391528c77696ae84de
ncs-4-u <nimbus4consultancy@gmail.com>
2024-08-20 01:21:55 +0200

    js

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById("hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
